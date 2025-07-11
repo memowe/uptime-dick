215b0450a06e4c4cccedca8d0dcd9a3d76add553
Mirko Westermeier <mirko.westermeier@uni-muenster.de>
2025-07-11 10:47:05 +0200

    Add documentation explaining the changes extraction script

--- extract_changes.pl
+++ extract_changes.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/env perl
+#
+# Usage: perl extract_changes.pl CHANGELOG.md v0.1.0.2
+# Extraction script to get changes matching a given version
+# from the changelog.
+# Only used by the continuous release workflow.
 
 use strict;
 use warnings;
